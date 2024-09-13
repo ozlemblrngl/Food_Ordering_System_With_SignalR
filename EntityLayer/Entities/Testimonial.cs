2a161e1b2a688031021727c082e3f06feafa7315
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-14 01:06:54 +0300

    basic entities added

---
+++ EntityLayer/Entities/Testimonial.cs
@@ -0,0 +1,19 @@
+﻿namespace EntityLayer.Entities
+{
+    public class Testimonial
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Comment { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool Status { get; set; }
+
+
+    }
+}
