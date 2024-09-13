136d52b769b829d7df3c6b6de6c8c7c955110ed0
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-14 00:55:10 +0300

    new classes added

---
+++ SignalRApi/DAL/Entities/Booking.cs
@@ -0,0 +1,17 @@
+﻿namespace SignalRApi.DAL.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Mail { get; set; }
+
+        public int PersonCount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ SignalRApi/DAL/Entities/Discount.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalRApi.DAL.Entities
+{
+    public class Discount
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Amount { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
