bea54e9a160235184a6f5c4cb72f7367821a7d22
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-15 17:31:51 +0300

    automapper added and all mapping done for all entity

---
+++ SignalRApi/Mapping/BookingMapping.cs
@@ -0,0 +1,20 @@
+﻿
+using AutoMapper;
+using DtoLayer.BookingDto;
+using EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class BookingMapping : Profile
+    {
+        public BookingMapping()
+        {
+            CreateMap<Booking, ResultBookingDto>().ReverseMap();
+            CreateMap<Booking, ResultBookingDto>().ReverseMap();
+            CreateMap<Booking, CreateBookingDto>().ReverseMap();
+            CreateMap<Booking, UpdateBookingDto>().ReverseMap();
+            CreateMap<Booking, GetBookingDto>().ReverseMap();
+        }
+
+    }
+}
---
+++ SignalRApi/Mapping/DiscountMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DtoLayer.DiscountDto;
+using EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class DiscountMapping : Profile
+    {
+        public DiscountMapping()
+        {
+            CreateMap<Discount, ResultDiscountDto>().ReverseMap();
+            CreateMap<Discount, CreateDiscountDto>().ReverseMap();
+            CreateMap<Discount, UpdateDiscountDto>().ReverseMap();
+            CreateMap<Discount, GetDiscountDto>().ReverseMap();
+        }
+    }
+}
