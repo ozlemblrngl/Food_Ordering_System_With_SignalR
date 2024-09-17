5ec44881f845ee17227cdab5d8f1b1fcc566b8bb
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-17 15:37:46 +0300

    category listed from model with ui

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
 
 namespace SignalRWebUI.Controllers
 {
-    public class CategoryController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class CategoryController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public CategoryController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7122/api/Categories");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+
+				if (values != null)
+				{
+					return View(values);
+				}
+			}
+
+			return View(new List<ResultCategoryDto>());
+		}
+
+	}
 }
---
+++ SignalRWebUI/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalRWebUI.Dtos.CategoryDtos
+{
+	public class ResultCategoryDto
+	{
+
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public bool Status { get; set; }
+	}
+}
