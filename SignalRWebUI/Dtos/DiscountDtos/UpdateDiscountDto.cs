e93fb0917fa38223022d8319bf1571ee8e9ba52d
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-19 17:23:40 +0300

    discount controller dtos views added to uı and sidebar

---
+++ SignalRWebUI/Dtos/DiscountDtos/UpdateDiscountDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalRWebUI.Dtos.DiscountDtos
+{
+	public class UpdateDiscountDto
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public int Amount { get; set; }
+
+		public string Description { get; set; }
+
+		public string ImageUrl { get; set; }
+	}
+}
