0e9e918b7c9ce73115f74bfb579abd16f82b0327
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-23 14:50:10 +0300

    using cors policy

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -46,14 +46,28 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//use cors
+builder.Services.AddCors(opt =>
+{
+	opt.AddPolicy("CorsPolicy", builder =>
+	{
+		builder.AllowAnyHeader()
+		.AllowAnyMethod()
+		.AllowAnyOrigin()
+		.AllowCredentials();
+	});
+});
+
+builder.Services.AddSignalR();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy"); //use cors
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
