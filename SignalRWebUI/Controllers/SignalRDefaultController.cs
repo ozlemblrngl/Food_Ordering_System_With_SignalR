5852c3e90ed788ab48d1470e6275083604f08355
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-24 11:42:02 +0300

    setınterval added and all updated category infos brought

--- SignalRWebUI/Controllers/SignalRDefaultController.cs
+++ SignalRWebUI/Controllers/SignalRDefaultController.cs
@@ -2,11 +2,16 @@
 
 namespace SignalRWebUI.Controllers
 {
-	public class SignalRDefaultController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class SignalRDefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
 }
