17c4bb29a2fc36d86dee7f008ac64afd5b4dba9f
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-27 12:24:25 +0300

    orderdetail datalayer and business layer added

---
+++ BusinessLayer/Concrete/OrderDetailManager.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Entities;
+
+namespace BusinessLayer.Concrete
+{
+	public class OrderDetailManager : IOrderDetailService
+	{
+		private readonly IOrderDetailDal _orderDetailDal;
+
+		public OrderDetailManager(IOrderDetailDal orderDetailDal)
+		{
+			_orderDetailDal = orderDetailDal;
+		}
+
+		public void TAdd(OrderDetail entity)
+		{
+			_orderDetailDal.Add(entity);
+		}
+
+		public void TDelete(OrderDetail entity)
+		{
+			_orderDetailDal.Delete(entity);
+		}
+
+		public OrderDetail TGetById(int id)
+		{
+			return _orderDetailDal.GetById(id);
+		}
+
+		public List<OrderDetail> TGetList()
+		{
+			return _orderDetailDal.GetList();
+		}
+
+		public void TUpdate(OrderDetail entity)
+		{
+			_orderDetailDal.Update(entity);
+		}
+	}
+}
---
+++ DataAccessLayer/EntityFramework/EfOrderDetail.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Repositories;
+using EntityLayer.Entities;
+
+namespace DataAccessLayer.EntityFramework
+{
+	public class EfOrderDetail : GenericRepository<OrderDetail>, IGenericDal<OrderDetail>, IOrderDetailDal
+	{
+		public EfOrderDetail(SignalRContext context) : base(context)
+		{
+		}
+	}
+}
